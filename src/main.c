e26f6a916c1c64403e614c5cacecac817ffd1f3d
Mohamed Dewidar <mkdewidar@gmail.com>
2017-12-26 09:58:01 +0000

    Add parser and parse input

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "mpc.h"
 
 static char input[2048];
 
@@ -7,14 +8,36 @@ int main(int argc, char** argv) {
   puts("Welcome to this (so far) untitled Lisp");
   puts("Press Ctrl+c to exit\n");
 
+  mpc_parser_t* number = mpc_new("number");
+  mpc_parser_t* operator = mpc_new("operator");
+  mpc_parser_t* expr = mpc_new("expr");
+  mpc_parser_t* code = mpc_new("code");
+
+  mpca_lang(MPCA_LANG_DEFAULT,
+	    "number: /-?[0-9]+/; \
+             operator: '+' | '-' | '*' | '/'; \
+             expr: <number> | '(' <operator> <expr>+ ')'; \
+             code: /^/ <operator> <expr>+ /$/;",
+	    number, operator, expr, code);
+
+
   while (1) {
     fputs("lisp> ", stdout);
     fflush(stdout);
     
     char* x = fgets(input, 2048, stdin);
 
-    printf("%s", input);
+    mpc_result_t r;
+    if (mpc_parse("<stdin>", input, code, &r)) {
+      mpc_ast_print(r.output);     
+      mpc_ast_delete(r.output);
+    } else {
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
   }
+
+  mpc_cleanup(4, number, operator, expr, code);
 
   return 0;
 }
