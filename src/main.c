db2600ecd913d09c33b660b2d4b6e2cad024f4b9
Mohamed Dewidar <mkdewidar@gmail.com>
2018-02-05 23:00:04 +0000

    Add ability to define lambdas and have local scope

--- src/main.c
+++ src/main.c
@@ -4,8 +4,10 @@
 static char input[2048];
 
 typedef struct lval lval;
+typedef struct env env;
 
 typedef struct env {
+  env* parent;
   int size;
   char** labels;
   lval** values;
@@ -23,7 +25,12 @@ typedef struct lval {
 
     char* symbol;
 
-    lbuiltin func;
+    struct {
+      lbuiltin builtin;
+      env* scope;
+      lval* params;
+      lval* body;
+    };
 
     struct {
       int count;
@@ -39,15 +46,17 @@ enum { LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_FUNC, LVAL_SEXPR, LVAL_QEXPR };
 #define LERR_BAD_NUM "Invalid number"
 #define LERR_UNDEFINED_SYMBOL "Undefined Symbol"
 #define LERR_NOT_VALID_SEXPR "Invalid sexpr, first element is not a function"
-#define LERR_TOO_MANY_ARGS(funcname) strcat(strcat("Function ", funcname), " has too many args")
-#define LERR_TOO_FEW_ARGS(funcname) strcat(strcat("Function ", funcname), " has too few args")
-#define LERR_INCORRECT_ARGS_TYPES(funcname) strcat(strcat("Function ", funcname), " passed incorrect types")
+#define LERR_TOO_MANY_ARGS(funcname) "Function " funcname " has too many args"
+#define LERR_TOO_FEW_ARGS(funcname) "Function " funcname " has too few args"
+#define LERR_INCORRECT_ARGS_TYPES(funcname) "Function " funcname " passed incorrect types"
 #define LERR_CANNOT_DEFINE_NON_SYM "Invalid name used to define symbol"
 #define LERR_DEF_SYM_VAL_MISMATCH "Mismatch between number of symbols and values provided"
+#define LERR_LAMBDA_ARGS_COUNT "Lambda definition must have parameter and body definition"
 
-lval* eval_sexpr(lval* v);
-lval* eval(lval* v);
+lval* eval_sexpr(env* e, lval* v);
+lval* eval(env* e, lval* v);
 lval* read(mpc_ast_t* tree);
+lval* call(env* e, lval* f, lval* args);
 
 void add_builtin(char* name, lbuiltin func);
 void add_all_builtins();
@@ -62,6 +71,7 @@ lval* builtin_array(env* e, lval* args);
 lval* builtin_eval(env* e, lval* args);
 lval* builtin_concat(env* e, lval* args);
 lval* builtin_def(env* e, lval* args);
+lval* builtin_lambda(env* e, lval* args);
 
 lval* lval_num(long n);
 lval* lval_err(char* code);
@@ -69,6 +79,7 @@ lval* lval_sym(char* symbol);
 lval* lval_sexpr(void);
 lval* lval_qexpr(void);
 lval* lval_func(lbuiltin func);
+lval* lval_lambda(env* parent, lval* params, lval* body);
 lval* lval_take(lval* v, int i);
 lval* lval_pop(lval* v, int i);
 void lval_add(lval* sexpr, lval* addition);
@@ -79,7 +90,8 @@ void lval_print(lval* v);
 void lval_println(lval* v);
 void lval_print_expr(lval* v, char open, char close);
 
-env* env_create();
+env* env_create(env* parent);
+env* env_copy(env* e);
 void env_delete(env* e);
 void env_put(env* e, char* key, lval* val);
 lval* env_get(env* e, lval* key);
@@ -89,7 +101,7 @@ env* e = NULL;
 
 int main(int argc, char** argv) {
 
-  e = env_create();
+  e = env_create(NULL);
   if (e == NULL) {
     fputs("ERROR: Failed to create environment, quitting...", stdout);
     return -1;
@@ -131,7 +143,7 @@ int main(int argc, char** argv) {
       // putchar('\n');
       // fflush(stdout);
       
-      expr = eval(expr);
+      expr = eval(e, expr);
       lval_println(expr);
 
       lval_del(expr);
@@ -179,21 +191,21 @@ lval* read(mpc_ast_t* tree) {
   return expressions;
 }
 
-lval* eval(lval* v) {
+lval* eval(env* e, lval* v) {
   if (v->type == LVAL_SYM) {
     lval* r = env_get(e, v);
     lval_del(v);
     return r;
   }
   if (v->type == LVAL_SEXPR) {
-    return eval_sexpr(v);
+    return eval_sexpr(e, v);
   }
   return v;
 }
 
-lval* eval_sexpr(lval* v) {
+lval* eval_sexpr(env* e, lval* v) {
   for (int i = 0; i < v->count; i++) {
-    v->exprs[i] = eval(v->exprs[i]);
+    v->exprs[i] = eval(e, v->exprs[i]);
   }
 
   for (int i = 0; i < v->count; i++) {
@@ -217,9 +229,40 @@ lval* eval_sexpr(lval* v) {
     return lval_err(LERR_NOT_VALID_SEXPR);
   }
 
-  lval* result = f->func(e, v);
+  lval* result = call(e, f, v);
   lval_del(f);
   return result;
+}
+
+lval* call(env* e, lval* f, lval* args) {
+  if (f->builtin != NULL) {
+    return f->builtin(e, args);
+  }
+
+  while (args->count) {
+    if (f->params->count == 0) {
+      lval_del(args);
+      return lval_err(LERR_TOO_MANY_ARGS("function call"));
+    }
+
+    lval* symbol = lval_pop(f->params, 0);
+    lval* value = lval_pop(args, 0);
+
+    env_put(f->scope, symbol->symbol, value);
+
+    lval_del(symbol);
+    lval_del(value);
+  }
+
+  lval_del(args);
+
+  if (f->params->count == 0) {
+    lval* b = lval_sexpr();
+    lval_add(b, lval_copy(f->body));
+    return builtin_eval(f->scope, b);
+  } else {
+    return lval_copy(f);
+  }
 }
 
 void add_builtin(char* name, lbuiltin func) {
@@ -234,6 +277,7 @@ void add_all_builtins() {
   add_builtin("concat", builtin_concat);
   add_builtin("eval", builtin_eval);
   add_builtin("def", builtin_def);
+  add_builtin("\\", builtin_lambda);
 
   add_builtin("+", builtin_add);
   add_builtin("-", builtin_sub);
@@ -366,7 +410,7 @@ lval* builtin_eval(env* e, lval* args) {
   lval* x = lval_pop(args, 0);
   lval_del(args);
   x->type = LVAL_SEXPR;
-  return eval(x);
+  return eval(e, x);
 }
 
 /* Given a sexpr with multiple qexprs as its children, will combine the qexprs to a single one */
@@ -421,6 +465,36 @@ lval* builtin_def(env* e, lval* args) {
   return lval_sexpr();
 }
 
+lval* builtin_lambda(env* e, lval* args) {
+  if (args->count != 2) {
+    lval_del(args);
+    return lval_err(LERR_LAMBDA_ARGS_COUNT);
+  }
+
+  if (args->exprs[0]->type != LVAL_QEXPR) {
+    lval_del(args);
+    return lval_err(LERR_INCORRECT_ARGS_TYPES("lambda"));
+  }
+
+  if (args->exprs[1]->type != LVAL_QEXPR) {
+    lval_del(args);
+    return lval_err(LERR_INCORRECT_ARGS_TYPES("lambda"));
+  }
+
+  for (int i = 0; i < args->exprs[0]->count; i++) {
+    if (args->exprs[0]->exprs[i]->type != LVAL_SYM) {
+      lval_del(args);
+      return lval_err(LERR_CANNOT_DEFINE_NON_SYM);
+    }
+  }
+
+  lval* params = lval_pop(args, 0);
+  lval* body = lval_pop(args, 0);
+  lval_del(args);
+
+  return lval_lambda(e, params, body);
+}
+
 lval* lval_num(long n) {
   lval* v = malloc(sizeof(lval));
   v->type = LVAL_NUM;
@@ -463,7 +537,19 @@ lval* lval_qexpr(void) {
 lval* lval_func(lbuiltin func) {
   lval* v = malloc(sizeof(lval));
   v->type = LVAL_FUNC;
-  v->func = func;
+  v->builtin = func;
+  return v;
+}
+
+lval* lval_lambda(env* parent, lval* params, lval* body) {
+  lval* v = malloc(sizeof(lval));
+  v->type = LVAL_FUNC;
+  v->builtin = NULL;
+  
+  v->scope = env_create(parent);
+
+  v->params = params;
+  v->body = body;
   return v;
 }
 
@@ -514,6 +600,11 @@ void lval_del(lval* v) {
     break;
 
   case LVAL_FUNC:
+    if (!v->builtin) {
+      lval_del(v->params);
+      lval_del(v->body);
+      env_delete(v->scope);
+    }
     break;
   }
 
@@ -549,7 +640,14 @@ lval* lval_copy(lval* v) {
     break;
 
   case LVAL_FUNC:
-    copy->func = v->func;
+    if (v->builtin != NULL) {
+      copy->builtin = v->builtin;
+    } else {
+      copy->builtin = NULL;
+      copy->params = lval_copy(v->params);
+      copy->body = lval_copy(v->body);
+      copy->scope = env_copy(v->scope);
+    }
     break;
   }
 
@@ -579,7 +677,15 @@ void lval_print(lval* v) {
     break;
 
   case LVAL_FUNC:
-    printf("<function>");
+    if (v->builtin) {
+      printf("<function>");
+    } else {
+      printf("(\\ ");
+      lval_print(v->params);
+      putchar(' ');
+      lval_print(v->body);
+      putchar(')');
+    }
     break;
   }
 }
@@ -603,12 +709,30 @@ void lval_print_expr(lval* v, char open, char close) {
   putchar(close);
 }
 
-env* env_create() {
+env* env_create(env* parent) {
   env* e = malloc(sizeof(env));
+  e->parent = parent;
   e->size = 0;
   e->labels = NULL;
   e->values = NULL;
   return e;
+}
+
+env* env_copy(env* e) {
+  env* copy = malloc(sizeof(env));
+  copy->size = e->size;
+
+  copy->parent = e->parent;
+  copy->labels = malloc(sizeof(char**) * copy->size);
+  copy->values = malloc(sizeof(lval**) * copy->size);
+
+  for (int i = 0; i < copy->size; i++) {
+    copy->labels[i] = malloc(strlen(e->labels[i]) + 1);
+    strcpy(copy->labels[i], e->labels[i]);
+    copy->values[i] = lval_copy(e->values[i]);
+  }
+
+  return copy;
 }
 
 void env_delete(env* e) {
@@ -650,5 +774,9 @@ lval* env_get(env* e, lval* key) {
     }
   }
 
-  return lval_err(LERR_UNDEFINED_SYMBOL);
+  if (e->parent) {
+    return env_get(e->parent, key);
+  } else {
+    return lval_err(LERR_UNDEFINED_SYMBOL);    
+  }
 }
